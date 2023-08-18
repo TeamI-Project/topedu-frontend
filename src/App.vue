b4fe66bc41cc418ad92dbeb404967528cb32b3bf
sonbaejun <sonqowns2@naver.com>
2023-08-18 16:01:58 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LoginComponent from "./components/LoginComponent.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {};
+  },
+  components: { LoginComponent },
+};
 </script>
 
-<style>
+<style scoped>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // npm run build 타겟 디렉토리 (백엔드쪽!)
+  outputDir: "../study/src/main/resources/static",
+
+  // npm run server 개발 진행시 포트가 다르기 때문에 프록시 설정해줘야 함
+  // target : 백엔드 port가 들어감
+  // changeOrigin -> true로 해야 cros 문제 해결할 수 있음
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       target: "http://localhost:8080",
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // }
 })
+
+// package.json script 파일 빌드용
+// "scripts": {
+//   "serve": "vue-cli-service serve --port 3000",
+//   "build": "vue-cli-service build --watch",
+//   "lint": "vue-cli-service lint"
+// },
