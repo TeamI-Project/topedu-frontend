6b0fe36debb16bac0952dd8cfcd768022cfccc1f
sonbaejun <sonqowns2@naver.com>
2023-08-18 16:35:38 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   // npm run build 타겟 디렉토리 (백엔드쪽!)
-  outputDir: "../study/src/main/resources/static",
-
   // npm run server 개발 진행시 포트가 다르기 때문에 프록시 설정해줘야 함
   // target : 백엔드 port가 들어감
   // changeOrigin -> true로 해야 cros 문제 해결할 수 있음
